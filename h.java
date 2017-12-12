4766ade94420bf14ad8bc28547f6f10d6ef6de3f
HaMzaBouRouiSsa <34230840+HaMzaBouRouiSsa@users.noreply.github.com>
2017-12-12 14:15:20 +0100

    Update h.java

--- h.java
+++ h.java
@@ -1,2 +1,29 @@
-import java.util.ArrayList;
+package Servlets;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MyDb 
+{
+    Connection con;
+    public Connection getCon()
+    {
+        try 
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sad_bnf", "root", "");
+        }
+        catch (ClassNotFoundException ex) 
+        {
+            Logger.getLogger(MyDb.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        catch (SQLException ex) 
+        {
+            Logger.getLogger(MyDb.class.getName()).log(Level.SEVERE, null, ex);
+        }        
+        return con;
+    }    
+}
