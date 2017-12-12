157b2c09b0d25a8481285ae6ce9b54c22b8fd9d5
smailrezgui <smailrezgui@yahoo.com>
2017-12-12 14:20:23 +0100

    Update h.java

--- h.java
+++ h.java
@@ -14,7 +14,7 @@ public class MyDb
         try 
         {
             Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/jj", "root", "");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hello there every body hahahahahaahahahahah", "root", "");
         }
         catch (ClassNotFoundException ex) 
         {
